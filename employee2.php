18322371bf9bc4d5c375a7464aec719d0f9f69fd
Techyuvaraj <yuvarajsankar748@gmail.com>
2026-06-22 07:03:15 +0530

    Create employee2.php

---
+++ employee2.php
@@ -0,0 +1,31 @@
+[
+    [],
+    {
+        "Cid": "cbt04",
+        "Eid": "cbte01",
+        "designation": "software engineer",
+        "salary": "300000"
+    },
+    {
+        "Cid": "cbt03",
+        "Eid": "cbte02",
+        "designation": "software engineer",
+        "salary": "270000"
+    },
+    {
+        "Cid": "cbt01",
+        "Eid": "cbte01",
+        "designation": "software engineer",
+        "salary": "350000"
+    },
+    {
+        "Eid": "cbte04",
+        "Designation": "software tester",
+        "Salary": "300000"
+    },
+    {
+        "Eid": "cbte05",
+        "Designation": "data analysit",
+        "Salary": "250000"
+    }
+]
